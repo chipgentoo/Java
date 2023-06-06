03f52efac17c2d3ad28b528563220a760fc3f2b2
chipgentoo <chip.gentoo@gmail.com>
2023-06-07 00:23:09 +0300

    Java-Лекция_1я-Задача_3

--- HomeWork/Lections/Lection1Task3.java
+++ HomeWork/Lections/Lection1Task3.java
@@ -1,4 +1,12 @@
 package Lections;
-
 public class Lection1Task3 {
+    public static void main(String[] args) {
+        String word = "Добро пожаловать на курс по Java";
+        String[] s = word.split(" ");
+        String out = " ";
+        for (int i = s.length - 1; i >= 0; i--) {
+            out += s[i] + " ";
+        }
+        System.out.println(out);
+    }
 }
