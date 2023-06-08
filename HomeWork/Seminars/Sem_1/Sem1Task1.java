fe4b4ff40cc93b617e2f8b16d7107a71359b1965
chipgentoo <chip.gentoo@gmail.com>
2023-06-08 14:40:10 +0300

    Минимальное и максимальное значение в массиве

--- HomeWork/Seminars/Sem_1/Sem1Task1.java
+++ HomeWork/Seminars/Sem_1/Sem1Task1.java
@@ -3,40 +3,40 @@ package Seminars.Sem_1;
 import java.util.Arrays;
 import java.util.Random;
 
-// 1. Задать одномерный массив и найти в нем минимальный и максимальный элементы ;
+// 1. Задать одномерный массив и найти в нем минимальный и максимальный элементы;
 
 public class Sem1Task1 {
     public static int[] RandomArray() {
         Random rand = new Random(); // создаем объект класса Random
-        int length = rand.nextInt(100); // рандомная длина массива до 100 значений
-        int[] nums = new int[length];
+        int length = rand.nextInt(100); // рандомно установленная длина массива до 100 значений
+        int[] numbs = new int[length];
         for (int i = 0; i < length; i++) {
-            nums[i] = rand.nextInt(100); // заполняем каждый элемент случайным числом от 0 до 99
+            numbs[i] = rand.nextInt(100); // заполняем каждый элемент случайным числом от 0 до 99
         }
-        return nums;
+        return numbs;
     }
 
     public static int minValue(int[] arr) {
         int minVal = Integer.MAX_VALUE;
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] < minVal) minVal = arr[i];
+        for (int j : arr) {
+            if (j < minVal) minVal = j;
         }
         return minVal;
     }
     public static int maxValue(int[] arr) {
         int maxVal = Integer.MIN_VALUE;
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] > maxVal) maxVal = arr[i];
+        for (int j : arr) {
+            if (j > maxVal) maxVal = j;
         }
         return maxVal;
     }
     public static void main(String[] args) {
-        int[] nums = RandomArray();
-        System.out.println(Arrays.toString(nums));
+        int[] numbs = RandomArray();
+        System.out.println(Arrays.toString(numbs));
         System.out.println(
-                "\nОбщая длина массива = " + nums.length +
-                "\nМинимальное значение = " + minValue(nums) +
-                "\nМаксимальное значение = " + maxValue(nums)
+                "\nОбщая длина массива = " + numbs.length +
+                "\nМинимальное значение = " + minValue(numbs) +
+                "\nМаксимальное значение = " + maxValue(numbs)
         );
     }
 }
