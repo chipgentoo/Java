28aeefacb29c1cd05d31c914e52440a6e28a144f
chipgentoo <chip.gentoo@gmail.com>
2023-06-08 14:40:58 +0300

    Диагонали двумерного массива

--- HomeWork/Seminars/Sem_1/Sem1Task2.java
+++ HomeWork/Seminars/Sem_1/Sem1Task2.java
@@ -12,42 +12,44 @@ import java.util.Scanner;
 public class Sem1Task2 {
 
     public static void printArray(int[][] array){
-        for (int i = 0; i < array.length; i++) {
-            for (int j = 0; j < array[i].length; j++) {
-                System.out.print(array[i][j] + "\t");
+        int sizeArray = array.length;
+        for (int[] items : array) {
+            for (int j = 0; j < sizeArray; j++) {
+                System.out.print(items[j] + "\t");
             }
             System.out.println();
         }
     }
 
     public static void initZero(int[][] array){
-        for (int i = 0; i < array.length; i++) {
-            for (int j = 0; j < array[i].length; j++) {
+        int sizeArray = array.length;
+        for (int i = 0; i < sizeArray; i++) {
+            for (int j = 0; j < sizeArray; j++) {
                 array[i][j] = 0;
             }
         }
     }
-    public static void diagLeft(int[][] array){
+    public static void digLeft(int[][] array){
         for (int i = 0; i < array.length; i++) {
             array[i][i] = 1;
         }
     }
-    public static void diagRight(int[][] array){
+    public static void digRight(int[][] array){
         for (int i = 0; i < array.length; i++) {
             array[i][array.length -1 -i] = 1;
         }
     }
 
-    public static void diagCross(int[][] array){
+    public static void digCross(int[][] array){
         initZero(array);
-        diagLeft(array);
-        diagRight(array);
+        digLeft(array);
+        digRight(array);
     }
     public static void main(String[] args) {
         int size = 0;
         System.out.print("введите размер массива: ");
         Scanner scanner = new Scanner(System.in);
-        if (scanner != null && scanner.hasNextInt()) {
+        if (scanner.hasNextInt()) {
             size = scanner.nextInt();
         } else {
             System.out.println("Введено не верное значение");
@@ -59,17 +61,17 @@ public class Sem1Task2 {
         System.out.println();
 
         initZero(array);
-        diagLeft(array);
+        digLeft(array);
         printArray(array);
         System.out.println();
 
         initZero(array);
-        diagRight(array);
+        digRight(array);
         printArray(array);
         System.out.println();
 
         initZero(array);
-        diagCross(array);
+        digCross(array);
         printArray(array);
     }
 }
