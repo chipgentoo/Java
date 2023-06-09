25e3b5d1df7c4369248da3cbfe527235d1bfec92
chipgentoo <chip.gentoo@gmail.com>
2023-06-10 00:10:51 +0300

    Семинар 2 Задача 1

--- HomeWork/Seminars/Sem_2/Sem2Task1.java
+++ HomeWork/Seminars/Sem_2/Sem2Task1.java
@@ -1,7 +1,31 @@
 package Seminars.Sem_2;
 
-public class Sem2Task1 {
-    public static void main(String[] args) {
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.nio.charset.StandardCharsets;
 
+public class Sem2Task1 {
+    public static StringBuilder createdString(String str, int number){
+        StringBuilder someStr = new StringBuilder();
+        for (int i = 0; i < number; i++) {
+            someStr.append(str);
+        }
+        return someStr;
+    }
+    public static void main(String[] args) {
+        File file = new File("test.txt");
+        StringBuilder text = createdString("test", 100);
+
+        try (FileOutputStream fos = new FileOutputStream(file);
+             OutputStreamWriter osw = new OutputStreamWriter(fos, StandardCharsets.UTF_8))
+        {
+            osw.write(String.valueOf(text));
+            System.out.println("Successfully written data to the file");
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
---
+++ HomeWork/Seminars/Sem_2/Sem2Task2.java
@@ -0,0 +1,2 @@
+package Seminars.Sem_2;public class Sem2Task2 {
+}
