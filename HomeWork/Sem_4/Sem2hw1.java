c688d735f8728c60ba1f206d00209c2916dd650a
chipgentoo <chip.gentoo@gmail.com>
2023-06-19 18:47:12 +0300

    Sem4hw1

---
+++ HomeWork/Sem_4/Sem2hw1.java
@@ -0,0 +1,29 @@
+/**
+ * Задача: Реализовать консольное приложение, которое:
+ * 1. Принимает от пользователя и “запоминает” строки.
+ * 2. Если введено print, выводит строки так, чтобы последняя введенная была первой в списке, а первая - последней.
+ * 3. Если введено revert, удаляет предыдущую введенную строку из памяти.
+ * 4. Если введено exit, то программа завершается
+ * > - ввод в консоль (stdin),
+ * < - вывод на консоль (stdout)
+ * > java
+ * > python
+ * > c#
+ * > print
+ * < [c#, python, java]
+ * > revert
+ * > print
+ * < [python, java]
+ * > revert
+ * > revert
+ * > print
+ * < []
+ * > revert -> throw new NoSuchElementException
+ */
+package HomeWork.Sem_4;
+
+public class Sem2hw1 {
+    public static void main(String[] args) {
+
+    }
+}
