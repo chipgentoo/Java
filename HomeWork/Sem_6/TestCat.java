4f06a4ba6dd063f4997608a53e2680d777a00fe9
chipgentoo <chip.gentoo@gmail.com>
2023-06-23 22:02:41 +0300

    Тест класса котиков

--- HomeWork/Sem_6/TestCat.java
+++ HomeWork/Sem_6/TestCat.java
@@ -1,2 +1,26 @@
-package HomeWork.Sem_6;public class TestCat {
+package HomeWork.Sem_6;
+
+public class TestCat {
+    public static void main(String[] args) {
+        Cats cat1 = new Cats();
+        cat1.setName("Васька");
+        cat1.setColor("Рыжий");
+        cat1.setSpecies("Британец");
+        System.out.println(cat1.getName());
+        System.out.println(cat1.getColor());
+        System.out.println(cat1.getSpecies());
+        System.out.println(cat1);
+
+        Cats cat2 = new Cats();
+        cat2.setName("Васька");
+        cat2.setColor("Рыжий");
+        cat2.setSpecies("Британец");
+        System.out.println(cat2.getName());
+        System.out.println(cat2.getColor());
+        System.out.println(cat2.getSpecies());
+        System.out.println(cat2);
+
+        System.out.println(cat1.equals(cat2));
+    }
+
 }
