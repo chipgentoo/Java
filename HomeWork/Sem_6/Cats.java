6e2098359d2454e2dcc49b794f5dc9229fd5f9e8
chipgentoo <chip.gentoo@gmail.com>
2023-06-23 22:02:18 +0300

    Класс котиков

--- HomeWork/Sem_6/Cats.java
+++ HomeWork/Sem_6/Cats.java
@@ -1,2 +1,63 @@
-package HomeWork.Sem_6;public class cats {
+/**
+ * 1. Продумайте структуру класса Кот. Какие поля и методы будут актуальны для приложения, которое является
+ * а) информационной системой ветеринарной клиники
+ * б) архивом выставки котов
+ * в) информационной системой Театра кошек Ю. Д. Куклачёва
+ * Можно записать в текстовом виде, не обязательно реализовывать в java.
+ */
+package HomeWork.Sem_6;
+
+import java.util.Objects;
+
+public class Cats {
+    private String Name;
+    private String Color;
+    private String Species;
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Cats cats = (Cats) o;
+        return Objects.equals(Name, cats.Name) && Objects.equals(Color, cats.Color) && Objects.equals(Species, cats.Species);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(Name, Color, Species);
+    }
+
+    @Override
+    public String toString() {
+        return "Cats{" +
+                "Name='" + Name + '\'' +
+                ", Color='" + Color + '\'' +
+                ", Species='" + Species + '\'' +
+                '}';
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public String getColor() {
+        return Color;
+    }
+
+    public void setColor(String color) {
+        Color = color;
+    }
+
+    public String getSpecies() {
+        return Species;
+    }
+
+    public void setSpecies(String species) {
+        Species = species;
+    }
+
+
 }
