aa554a7378f45f183060c76ac2ea28c8b296723d
chipgentoo <chip.gentoo@gmail.com>
2023-06-29 02:02:03 +0300

    Pets as Main class

--- HomeWork/Part_2/Animals/Pets.java
+++ HomeWork/Part_2/Animals/Pets.java
@@ -2,7 +2,9 @@ package HomeWork.Part_2.Animals;
 
 public class Pets {
     public static void main(String[] args) {
-        Cat cat1 = new Cat("Васька");
+        Cat cat1 = new Cat("Кот");
+        Dog dog1 = new Dog("Пёс");
         System.out.println(cat1);
+        System.out.println(dog1);
     }
 }
