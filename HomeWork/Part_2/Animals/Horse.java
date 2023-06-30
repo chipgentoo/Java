8100b80422bcac2b160dc04dc4b1b1aaf8e05a36
chipgentoo <chip.gentoo@gmail.com>
2023-06-30 14:18:15 +0300

    class Horse extends Animals

---
+++ HomeWork/Part_2/Animals/Horse.java
@@ -0,0 +1,17 @@
+package Part_2.Animals;
+
+public class Horse extends Animals {
+    public Horse(){
+        super.animalType = enumAnimalType.HORSE;
+        super.animalName = "Лошадка_" + countAnimal;
+        super.animalVoice = "Иго-го";
+        super.animalAge = super.getAnimalAge();
+        super.volumeFeed = 1.2f;    // потребление еды за раз
+        super.animalWeight = 1.2f;  // начальный вес
+    }
+    @Override
+    public String toString() {
+        return  "Тип: " + animalType + ", " + super.toString();
+    }
+
+}
