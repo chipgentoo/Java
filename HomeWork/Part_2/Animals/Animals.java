afc53e92d085d967408801e98286a0ec58ee4418
chipgentoo <chip.gentoo@gmail.com>
2023-06-30 14:21:42 +0300

    Main

--- HomeWork/Part_2/Animals/Animals.java
+++ HomeWork/Part_2/Animals/Animals.java
@@ -1,41 +1,109 @@
 package Part_2.Animals;
 
+import java.util.Scanner;
+
 /**
  *  Абстрактный класс - Animals (Животные)
+ *  После рождения животное начинает свой жизненный цикл. В Main.
+ *  Периодически требуется его кормить.
+ *  Вес, Голод, Усталость, Сон, Движение, Кол-во пищи, Наполненность миски.
+ *  Если в миске еды больше чем съедаемое за раз, то цикл в автомате и кормить не нужно.
+ *  После сна +0.25 дня.
+ *  Потребление пищи за раз и начальный вес указываются в наследнике.
  */
 public abstract class Animals {
-    protected enum enumAnimalType{ CAT, DOG }
-    protected enumAnimalType animalType; // тип животного
-    protected String animalName;
-    protected float animalAge;
-    protected float animalWeight;
-    protected String animalVoice;
+    protected static int countAnimal = 0; // счетчик животных
+    protected enum enumAnimalType{ CAT, DOG, HORSE }
+    protected enumAnimalType animalType; // тип
+    protected String animalName; // кличка
+    protected float animalAge; // возраст
+    protected float animalWeight; // вес
+    protected String animalVoice; // голос
+
+
     // ========== ЕДА ==========
     protected float volumeFeed; // кол-во, съедаемое за раз
     protected int countFeeding; // кол-во кормлений
-    protected float eatVolume;  // кол-во съеденной еды
+    protected float eatVolume;  // общее кол-во съеденной еды
     protected float leftoverFood; // Остаток еды в кормушке
-    // ==========  ==========
+    protected boolean animalSatiety; // Сытость
+    protected boolean animalFatigue; // Усталость
+
+
+    // ========== ДЕЙСТВИЯ ==========
     protected boolean animalMove; // Гуляет / Играет / Двигается
     protected boolean animalSleep; // Сон / Спать
-    protected boolean animalSatiety; // Сытость
 
 
     // ========== РЕАЛИЗАЦИЯ ==========
     protected float getAnimalAge() { return animalAge; } // default 0;
     protected int animalCountView; // кол-во осмотров животного / контроль роста
 
-    public Animals() {
+    protected Animals() {
+        countAnimal += 1;
         this.animalAge = 0f;
         this.animalWeight = 0f;
         this.countFeeding = 0;
         this.eatVolume = 0f;
         this.animalMove = false;
         this.animalSleep= false;
-        this.animalSatiety = false;
+        this.animalSatiety = true;
+        this.animalFatigue = false;
         this.animalCountView = 0;
         this.leftoverFood = 0f;
     }
+    protected void toMove(){ // Движение
+        if (this.animalFatigue){ // если устали
+            System.out.println(this.animalName + ": Устали");
+            this.toFeed();  // поели
+            this.toSleep(); // поспали
+        } else {
+            this.animalMove = true;     // Двигаемся
+            System.out.println(this.animalName + ": Двигаемся");
+            this.animalFatigue = true;  // Устали
+        }
+    }
+    protected void toSleep(){ // Сон / Спать
+        if (this.animalFatigue){// если устали
+            this.animalSleep = true;    // уснули
+            this.animalFatigue = false; // полны сил
+            this.animalAge += 0.25f;    // подросли
+            System.out.println(this.animalName + ": Поспали");
+        }
+        this.toFeed();
+    }
+    protected void toFeed(float Value){                 // Кормление
+        this.leftoverFood += Value;                     // еды в кормушке
+        // если в кормушке больше чем съедаемое за раз
+        if (this.leftoverFood >= this.volumeFeed){
+            this.countFeeding += 1;                     // кол-во кормлений +1
+            this.eatVolume += volumeFeed;               // общее кол-во съеденной еды
+            this.leftoverFood -= this.volumeFeed;       // кормушка - съедаемое за раз
+            this.animalSatiety = true;                  // сытость
+            this.animalWeight += this.volumeFeed / 2;   // вес + половина съеденной за раз еды
+            System.out.println(this.animalName + ": Поели");
+        } else {
+            System.out.println(this.animalName + ": В кормушке мало еды");
+            this.animalSatiety = false;
+            this.toFeed();
+        }
+    }
+    protected void toFeed(){ // кормление
+        // если в миске больше съедаемого за раз
+        if(this.leftoverFood > volumeFeed){
+            this.toFeed(0f);
+        } else {
+            this.addEat();
+        }
+    }
+    protected void addEat(){ // добавление еды в кормушку
+        System.out.print("Добавьте еды в кормушку " + this.animalName + ": ");
+        Scanner scanner = new Scanner(System.in);
+        if (scanner.hasNextFloat()){
+            this.toFeed(scanner.nextFloat());
+        }
+    }
+
     @Override
     public String toString() {
         animalCountView += 1; // контроль роста
@@ -45,13 +113,7 @@ public abstract class Animals {
                 "Вес: " + animalWeight + " кг, " +
                 "Кол-во кормлений: " + countFeeding + ", " +
                 "Съедено еды: " + eatVolume + ", " +
-                "Сытость: " + animalSatiety;
-    }
-    protected void toFeed(float eatValue){
-        this.eatVolume += volumeFeed;
-        this.animalWeight = volumeFeed / 2;
-        this.countFeeding += 1;
-        this.animalSatiety = true;
-        System.out.println("Животное поело");
+                "Сытость: " + animalSatiety + ", " +
+                "Усталость: " + animalFatigue;
     }
 }
--- HomeWork/Part_2/Animals/Pets.java
+++ HomeWork/Part_2/Animals/Pets.java
@@ -1,10 +1,23 @@
 package Part_2.Animals;
 
+import java.util.ArrayList;
+
 public class Pets {
     public static void main(String[] args) {
-        Cat cat1 = new Cat("Кот");
-        System.out.println(cat1);
-        cat1.toFeed(200f);
-        System.out.println(cat1);
+        ArrayList<Animals> petList = new ArrayList<>();
+        int countPet = 1; // кол-во животных каждого вида
+        int countLife = 10; //кол-во жизненных циклов
+        for (int i = 0; i < countPet; i++) {
+            petList.add(new Cat());
+            petList.add(new Dog());
+            petList.add(new Horse());
+        }
+        for (int i = 0; i < countLife; i++) {
+            System.out.println("============================================");
+            for (Animals animals : petList) { System.out.println(animals); }
+            for (Animals animals : petList) { animals.toMove(); animals.toSleep();}
+            for (Animals animals : petList) { System.out.println(animals); }
+        }
     }
 }
+
