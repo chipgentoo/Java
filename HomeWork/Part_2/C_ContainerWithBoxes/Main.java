e41e91a12b2856fc5a3c21c5df7cbc7b64d69a91
chipgentoo <chip.gentoo@gmail.com>
2023-07-05 19:38:40 +0300

    Main

--- HomeWork/Part_2/C_ContainerWithBoxes/Main.java
+++ HomeWork/Part_2/C_ContainerWithBoxes/Main.java
@@ -1,28 +1,41 @@
-package Part_2.C_ContainerWithBoxes;
+package HomeWork.Part_2.C_ContainerWithBoxes;
 
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.Iterator;
 
 public class Main {
     public static void main(String[] args) {
-        Container container = new Container();
-        container.addBox(new Box(4L,24,"box 4"));
-        container.addBox(new Box(2L,11,"box 2"));
-        container.addBox(new Box(5L,52,"box 5"));
-        container.addBox(new Box(3L,37,"box 3"));
-        container.addBox(new Box(1L,49,"box 1"));
+        Container container1 = new Container("Контейнер 1");
+        container1.addBox(new Box(4L,24,"box 4"));
+        container1.addBox(new Box(2L,11,"box 2"));
+        container1.addBox(new Box(5L,52,"box 5"));
+        container1.addBox(new Box(3L,37,"box 3"));
+        container1.addBox(new Box(1L,49,"box 1"));
 
-        var boxes = container.getBoxList();
+        var boxes1 = container1.getBoxList();
 
-        boxes.sort(Comparator.naturalOrder());
-        //System.out.println(boxes);
+        Container container2 = new Container("Контейнер 2");
+        container2.addBox(new Box(9L,33,"box 9"));
+        container2.addBox(new Box(6L,25,"box 6"));
+        container2.addBox(new Box(8L,12,"box 8"));
+        container2.addBox(new Box(7L,71,"box 7"));
+        container2.addBox(new Box(0L,93,"box 0"));
 
-        for (Box box : boxes) {
+        var boxes2 = container2.getBoxList();
+
+
+        //boxes1.sort(Comparator.naturalOrder());
+        System.out.println(boxes1);
+        //boxes2.sort(Comparator.naturalOrder());
+        System.out.println(boxes2);
+
+        for (Box box : boxes1) {
             System.out.println(box);
         }
-        System.out.printf("Общий вес контейнера: %s%n", container.getWeight());
+        System.out.printf("Общий вес контейнера: %s%n", container1.getWeight());
 
-        Iterator<Box> boxIterator = container.iterator();
+        Iterator<Box> boxIterator = container1.iterator();
         while (boxIterator.hasNext()){
             Box box = boxIterator.next();
             //System.out.printf("ID:%s\tWeigh:%s\tName:%s%n", box.getId(),box.getWeight(),box.getBoxName());
@@ -31,9 +44,19 @@ public class Main {
                 System.out.println("Удалили box 2");
             }
         }
-        for (Box box : boxes) {
+        for (Box box : boxes1) {
             System.out.println(box);
         }
-        System.out.printf("Общий вес контейнера: %s", container.getWeight());
+        System.out.printf("Общий вес контейнера 1: %s%n", container1.getWeight());
+        System.out.printf("Общий вес контейнера 2: %s%n", container2.getWeight());
+
+        // Сравнение контейнеров
+        //System.out.println(container1.compareTo(container2));
+        switch (container1.compareTo(container2)) {
+            case -1 -> System.out.println(container1.containerName + " весит меньше чем " + container2.getContainerName());
+            case 0 ->  System.out.println("Вес контейнеров одинаковый");
+            case 1 ->  System.out.println(container1.containerName + " весит больше чем " + container2.getContainerName());
+        }
+
     }
 }
