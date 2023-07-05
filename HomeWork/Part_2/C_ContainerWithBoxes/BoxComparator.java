de2aaa3b425569ed5276cfbfbccc86e08329f042
chipgentoo <chip.gentoo@gmail.com>
2023-07-05 14:29:28 +0300

    Class BoxComparator

--- HomeWork/Part_2/C_ContainerWithBoxes/BoxComparator.java
+++ HomeWork/Part_2/C_ContainerWithBoxes/BoxComparator.java
@@ -1,9 +1,8 @@
 package Part_2.C_ContainerWithBoxes;
 
 import java.util.Comparator;
-import java.util.List;
 
-public class BoxCompatator implements Comparator<Box>{
+public class BoxComparator implements Comparator<Box>{
     @Override
     public int compare(Box o1, Box o2) {
         String boxName1 = o1.getBoxName().split("\\s+")[1];
