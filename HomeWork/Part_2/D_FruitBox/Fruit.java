1afa95277f5cbfb4c3ceb0785825ada8a43ef3b6
chipgentoo <chip.gentoo@gmail.com>
2023-07-09 21:42:00 +0300

    Abstract Class Fruit

---
+++ HomeWork/Part_2/D_FruitBox/Fruit.java
@@ -0,0 +1,13 @@
+package HomeWork.Part_2.D_FruitBox;
+
+public abstract class Fruit {
+    private final int weight;
+
+    public Fruit(int weight) {
+        this.weight = weight;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+}
